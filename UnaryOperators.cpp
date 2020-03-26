f75fe746c98729b8de2899a7a93d2e677451b6a7
Aryan Mukerji <aryanmukerji@gmail.com>
2020-03-27 00:29:22 +0530

    Post Decrement snipped added

--- UnaryOperators.cpp
+++ UnaryOperators.cpp
@@ -10,12 +10,19 @@ int main()
     cout << " b value after a++ : " << b << endl; 
     cout << " a value after a++ : " << a << endl;
 	
-	// Pre increment 
+    // Pre increment 
     a = 1; 
     cout << " a value : " << a << endl; 
     b = ++a; 
     cout << " b value after ++a : " << b << endl; 
     cout << " a value after ++a : " << a << endl;
+	
+	// Post decrement 
+    a = 5; 
+    cout << " a value before decrement : " << a << endl; 
+    b = a--; 
+    cout << " b value after a-- : " << b << endl; 
+    cout << " a value after a-- : " << a << endl;
 
     return 0;
 }	
