b2ad7eff6403097a195fe55f6911262867c2d09c
Aryan Mukerji <aryanmukerji@gmail.com>
2020-03-27 00:17:29 +0530

    Pre Increment snipped added

--- UnaryOperators.cpp
+++ UnaryOperators.cpp
@@ -9,6 +9,13 @@ int main()
     int b = a++; 
     cout << " b value after a++ : " << b << endl; 
     cout << " a value after a++ : " << a << endl;
+	
+	// Pre increment 
+    a = 1; 
+    cout << " a value : " << a << endl; 
+    b = ++a; 
+    cout << " b value after ++a : " << b << endl; 
+    cout << " a value after ++a : " << a << endl;
 
     return 0;
 }	
