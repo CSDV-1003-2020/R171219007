a9449cb14015d8a82a3fe318f67bd6e57662310d
Aryan Mukerji <aryanmukerji@gmail.com>
2020-03-27 00:12:12 +0530

    Post Increment snipped added

---
+++ UnaryOperators.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+using namespace std; 
+
+int main() 
+{ 
+    // Post increment 
+    int a = 1; 
+    cout << " a value : " << a << endl; 
+    int b = a++; 
+    cout << " b value after a++ : " << b << endl; 
+    cout << " a value after a++ : " << a << endl;
+
+    return 0;
+}	
+	
