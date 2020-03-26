32b23a6f1c752e5db64ca500b24c7d9a22fd7783
Aryan Mukerji <aryanmukerji@gmail.com>
2020-03-26 10:24:02 +0530

    Push from beginning of a Linked List

---
+++ pushlist.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+struct Node
+{
+    int info;
+    Node *next;
+    
+} *start, *newptr, *save, *ptr;
+
+Node *Create_New_Node(int n)
+{
+    ptr = new Node;
+    ptr->info = n;
+    ptr->next = NULL;
+    
+    return ptr;
+}
+
+void Insert_Beg(Node*);
+void Display(Node*);
+
+int main()
+{
+	
+	return 0;
+}	
