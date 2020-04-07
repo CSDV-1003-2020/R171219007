a7b0971ac05aea1b4693f5898c2e9bc45387f8c7
himanshu dubey <hd156681@gmail.com>
2020-04-07 12:25:53 +0530

    code completed

--- Inheritance/HierarchicalInheritance.cpp
+++ Inheritance/HierarchicalInheritance.cpp
@@ -1,21 +1,38 @@
-// Hierarchical Inheritance 
-#include <iostream> 
-using namespace std; 
-  
-// base class 
+#include <iostream>
+using namespace std;
+ 
+// base class
 class Vehicle  
-{ 
-  public: 
-    Vehicle() 
-    { 
-      cout << "This is a Vehicle" << endl; 
-    } 
-}; 
-  
-  
-// first sub lass  
-class Car: public Vehicle 
-{ 
-  
+{
+  public:
+    Vehicle()
+    {
+      cout << "This is a Vehicle" << endl;
+    }
 };
+ 
+ 
+// first sub class  
+class Car: public Vehicle
+{
+ 
+};
+ 
+// second sub class
+class Bus: public Vehicle
+{
+     
+};
+ 
+// main function
+int main()
+{    
+    // creating object of sub class will
+    // invoke the constructor of base class
+    Car obj1;
+    Bus obj2;
+    return 0;
+}
+
+
 
